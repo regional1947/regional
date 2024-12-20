e0f82391a58968768afbbe3633e28f74cafb463e
Big Elon <soft6dev@gmail.com>
2024-12-20 02:20:23 +0100

    best

--- src/main/java/bank/regional/app/user/User.java
+++ src/main/java/bank/regional/app/user/User.java
@@ -5,10 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import bank.regional.app.account.Account;
 
 @Entity
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
